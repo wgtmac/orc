wgtmac/orc

commit 481aadf4bbcaf2ebc36e862dafa6a628b94283d2

ORC-2011: [C++] Fix `Timezone` to support legacy `US` TimeZone identifiers
### What changes were proposed in this pull request?

This PR aims to fix `Timezone` to support legacy `US` TimeZone identifiers.

### Why are the changes needed?

Since `Ubuntu 24.04` and `Debian 13` doesn't provide old `/usr/share/zoneinfo/US/*` files, ORC C++ library fails with the following error by default. It's misleading because both recent `IANA timezone database` and `TZDIR` cannot resolve this issue. We had better provide a workaround via aliases.

> C++ exception with description "Time zone file /usr/share/zoneinfo/US/Pacific does not exist.
> Please install IANA time zone database and set TZDIR env." thrown in the test body.

Although there are many legacy timezone identifies, this PR aims to focus on `US` issues. For the rest of the code, we can handle it later based on the usage.

- https://data.iana.org/time-zones/tzdb/backward

### How was this patch tested?

Pass the CIs and manually run a docker test without these lines.

https://github.com/apache/orc/blob/fbea8e016699ad8e7b318f5c793b4e48fe85af57/docker/ubuntu24/Dockerfile#L58

https://github.com/apache/orc/blob/fbea8e016699ad8e7b318f5c793b4e48fe85af57/docker/debian13/Dockerfile#L40

I verified locally with the revised `Debian 13` image.

```
$ docker run -it --rm apache/orc-dev:debian13 ls -al /usr/share/zoneinfo/US
ls: cannot access '/usr/share/zoneinfo/US': No such file or directory

$ ./run-one.sh local x debian13
Started local run for ORC-2011 on debian13 at Fri Sep 26 21:54:25 PDT 2025
-- The C compiler identification is GNU 14.2.0
-- The CXX compiler identification is GNU 14.2.0
...

Test project /root/build
    Start 1: orc-test
1/9 Test #1: orc-test .........................   Passed    7.24 sec
    Start 2: java-test
2/9 Test #2: java-test ........................   Passed  110.33 sec
    Start 3: java-examples-test
3/9 Test #3: java-examples-test ...............   Passed    0.37 sec
    Start 4: java-tools-test

c++/src/Timezone.cc
@@ -33,8 +33,24 @@ namespace orc {
   // default location of the timezone files
   static const char DEFAULT_TZDIR[] = "/usr/share/zoneinfo";
 
-  // location of a symlink to the local timezone
-  static const char LOCAL_TIMEZONE[] = "/etc/localtime";
+  // location of a symlink to the local timezone is /etc/localtime
+  static const char LOCAL_TIMEZONE_DIR[] = "/etc";
+  static const char LOCAL_TIMEZONE[] = "localtime";
+
+  // US aliases from https://data.iana.org/time-zones/tzdb/backward
+  static const std::map<const std::string, const std::string> TZ_ALIASES = {
+      {"US/Alaska", "America/Anchorage"},
+      {"US/Aleutian", "America/Adak"},
+      {"US/Arizona", "America/Phoenix"},
+      {"US/Central", "America/Chicago"},
+      {"US/East-Indiana", "America/Indiana/Indianapolis"},
+      {"US/Eastern", "America/New_York"},
+      {"US/Hawaii", "Pacific/Honolulu"},
+      {"US/Indiana-Starke", "America/Indiana/Knox"},
+      {"US/Michigan", "America/Detroit"},
+      {"US/Mountain", "America/Denver"},
+      {"US/Pacific", "America/Los_Angeles"},
+      {"US/Samoa", "Pacific/Pago_Pago"}};
 
   enum TransitionKind { TRANSITION_JULIAN, TRANSITION_DAY, TRANSITION_MONTH };
 
@@ -734,14 +750,26 @@ namespace orc {
    * Get a timezone by absolute filename.
    * Results are cached.
    */
-  const Timezone& getTimezoneByFilename(const std::string& filename) {
+  const Timezone& getTimezoneByFilename(const std::string& dir, const std::string& zone) {
+    std::string filename(dir);
+    filename += "/";
+    filename += zone;
     // ORC-110
     std::lock_guard<std::mutex> timezone_lock(timezone_mutex);
     std::map<std::string, std::shared_ptr<Timezone> >::iterator itr = timezoneCache.find(filename);
     if (itr != timezoneCache.end()) {
       return *(itr->second).get();
     }
-    timezoneCache[filename] = std::make_shared<LazyTimezone>(filename);
+    auto it = TZ_ALIASES.find(zone);
+    if (it == TZ_ALIASES.end()) {
+      timezoneCache[filename] = std::make_shared<LazyTimezone>(filename);
+    } else {
+      std::string newfilename(dir);
+      newfilename += "/";
+      newfilename += it->second;
+      timezoneCache[newfilename] = std::make_shared<LazyTimezone>(newfilename);
+      timezoneCache[filename] = timezoneCache[newfilename];
+    }
     return *timezoneCache[filename].get();
   }
 
@@ -752,7 +780,7 @@ namespace orc {
 #ifdef _MSC_VER
     return getTimezoneByName("UTC");
 #else
-    return getTimezoneByFilename(LOCAL_TIMEZONE);
+    return getTimezoneByFilename(LOCAL_TIMEZONE_DIR, LOCAL_TIMEZONE);
 #endif
   }
 
@@ -761,10 +789,7 @@ namespace orc {
    * Results are cached.
    */
   const Timezone& getTimezoneByName(const std::string& zone) {
-    std::string filename(getTimezoneDirectory());
-    filename += "/";
-    filename += zone;
-    return getTimezoneByFilename(filename);
+    return getTimezoneByFilename(getTimezoneDirectory(), zone);
   }
 
   /**

commit 7f1dd1821bac79ccd017ca73d082bcaba1055b86

ORC-2215: [C++] Fix use-after-free bug in timezone cache
`getTimezoneByFilename` overwrites existing entries, potentially causing causing callers which already took a pointer to that entry to end up with a dangling pointer. I think the best fix is to only insert for new keys, since if the key already exists, the value is correct - this is a cache for a pure function. I think the issue may have been introduced in https://github.com/apache/orc/commit/481aadf4bbcaf2ebc36e862dafa6a628b94283d2 (ORC-2011).

I wrote a new unit test for the issue, `TimestampAliasCacheEviction.readerSurvivesAliasCacheEviction`. Without the fix, this test segfaults due to a dangling pointer. ASAN gives good diagnostics:

```
$ cmake /tmp/orc-tz-race \
      -DBUILD_JAVA=OFF -DBUILD_TOOLS=OFF -DBUILD_CPP_TESTS=ON \
      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
      -DCMAKE_CXX_FLAGS="-fsanitize=address" \
      -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address"
$ make -j$(nproc) orc-test
$ ASAN_OPTIONS=detect_leaks=0 /tmp/orc-tz-race-build-asan/c++/test/orc-test --gtest_filter='TimestampAliasCacheEviction*'
...
[ RUN      ] TimestampAliasCacheEviction.readerSurvivesAliasCacheEviction
=================================================================
==917813==ERROR: AddressSanitizer: heap-use-after-free on address 0xffff94413b30 at pc 0x000000e65bf4 bp 0xfffff4e1ce60 sp 0xfffff4e1ce50
READ of size 8 at 0xffff94413b30 thread T0
    #0 0xe65bf0 in orc::TimestampColumnReader::next(orc::ColumnVectorBatch&, unsigned long, char*) /tmp/orc-tz-race/c++/src/ColumnReader.cc:336
    #1 0xe65208 in void orc::StructColumnReader::nextInternal<false>(orc::ColumnVectorBatch&, unsigned long, char*) /tmp/orc-tz-race/c++/src/ColumnReader.cc:868
    #2 0xe65208 in orc::StructColumnReader::next(orc::ColumnVectorBatch&, unsigned long, char*) /tmp/orc-tz-race/c++/src/ColumnReader.cc:849
    #3 0xc9a700 in orc::RowReaderImpl::next(orc::ColumnVectorBatch&) /tmp/orc-tz-race/c++/src/Reader.cc:1417

c++/src/Timezone.cc
@@ -773,7 +773,12 @@ namespace orc {
       std::string newfilename(dir);
       newfilename += "/";
       newfilename += it->second;
-      timezoneCache[newfilename] = std::make_shared<LazyTimezone>(newfilename);
+      // Only insert if absent: overwriting an existing entry destroys the
+      // LazyTimezone while raw pointers in live TimestampColumnReaders still
+      // reference it, causing a use-after-free (SIGSEGV) on the next read.
+      if (timezoneCache.find(newfilename) == timezoneCache.end()) {
+        timezoneCache[newfilename] = std::make_shared<LazyTimezone>(newfilename);
+      }
       timezoneCache[filename] = timezoneCache[newfilename];
     }
     return *timezoneCache[filename].get();

c++/test/TestWriter.cc
@@ -2648,6 +2648,47 @@ namespace orc {
     }
   }
 
+  TEST(TimestampAliasCacheEviction, readerSurvivesAliasCacheEviction) {
+    MemoryOutputStream memStream(DEFAULT_MEM_STREAM_SIZE);
+    MemoryPool* pool = getDefaultPool();
+    std::unique_ptr<Type> type(Type::buildTypeFromString("struct<ts:timestamp>"));
+
+    // Write 2000 rows so that reading with a 1024-row batch requires two next() calls.
+    const uint64_t rowCount = 2000;
+    std::unique_ptr<Writer> writer =
+        createWriter(64 * 1024 * 1024, 64 * 1024, 64 * 1024, CompressionKind_ZLIB, *type, pool,
+                     &memStream, FileVersion::v_0_12(), 0, "America/New_York");
+    std::unique_ptr<ColumnVectorBatch> batch = writer->createRowBatch(rowCount);
+    StructVectorBatch* structBatch = dynamic_cast<StructVectorBatch*>(batch.get());
+    TimestampVectorBatch* tsBatch = dynamic_cast<TimestampVectorBatch*>(structBatch->fields[0]);
+    for (uint64_t i = 0; i < rowCount; ++i) {
+      tsBatch->data[i] = static_cast<int64_t>(i * 3600);
+      tsBatch->nanoseconds[i] = 0;
+    }
+    structBatch->numElements = rowCount;
+    tsBatch->numElements = rowCount;
+    writer->add(*batch);
+    writer->close();
+
+    auto inStream = std::make_unique<MemoryInputStream>(memStream.getData(), memStream.getLength());
+    std::unique_ptr<Reader> reader = createReader(pool, std::move(inStream));
+    std::unique_ptr<RowReader> rowReader = createRowReader(reader.get(), "GMT");
+    ASSERT_EQ(rowCount, reader->getNumberOfRows());
+
+    std::unique_ptr<ColumnVectorBatch> readBatch = rowReader->createRowBatch(1024);
+
+    // First next() opens the stripe, constructs TimestampColumnReader, and stores
+    // writerTimezone_ = &getTimezoneByName("America/New_York").
+    ASSERT_TRUE(rowReader->next(*readBatch));
+    ASSERT_EQ(1024u, readBatch->numElements);
+
+    // Populate aliases in the timezone cache
+    (void)getTimezoneByName("US/Eastern");
+
+    // Verify that writerTimezone_ is still a valid pointer
+    EXPECT_TRUE(rowReader->next(*readBatch));
+  }
+
   std::vector<TestParams> testParams = {{FileVersion::v_0_11(), true},
                                         {FileVersion::v_0_11(), false},
                                         {FileVersion::v_0_12(), false},